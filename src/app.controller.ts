55d1f746254efb99274f6fdd107ba8a2bcb43109
Bushynska357 <77163973+Bushynska357@users.noreply.github.com>
2021-03-31 18:42:00 +0300

    test protected routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RequestModel } from './auth/request.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return 'Hello';
+  getHello(@Req()req: RequestModel): string {
+    return this.appService.getHello(req.user?.fullname);
   }
 }
